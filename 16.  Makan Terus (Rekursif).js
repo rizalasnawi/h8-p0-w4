df37edeb48ff90a5ab2dc35ce939d4aa2def06de
rizalasnawi <44157537+rizalasnawi@users.noreply.github.com>
2018-11-11 00:10:21 +0700

    rekursif

---
+++ 16.  Makan Terus (Rekursif).js
@@ -0,0 +1,16 @@
+function makanTerusRekursif(waktu) {
+  // you can only write your code here!
+  var hasil = 15
+  if(waktu <= 0){
+      return 0;
+  }else{
+      return 1+makanTerusRekursif(waktu-hasil)
+  }
+}
+
+// TEST CASES
+console.log(makanTerusRekursif(66)); // 5
+console.log(makanTerusRekursif(100)); // 7
+console.log(makanTerusRekursif(90)); // 6
+console.log(makanTerusRekursif(10)); // 1
+console.log(makanTerusRekursif(0)); // 0
