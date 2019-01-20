b842751e7833a5d7ed7c18b242a337049f9813c6
rizalasnawi <44157537+rizalasnawi@users.noreply.github.com>
2019-01-20 16:26:57 +0700

    update

--- 7. Digit Perkalian Minimum.js
+++ 7. Digit Perkalian Minimum.js
@@ -1,32 +1,28 @@
 function digitPerkalianMinimum(angka) {
-    // you can only write your code here!
-    //var str=String(angka);
-    var temp=""
-    var pengali=[]
-
-    for(var i=1;i<=angka;i++){
-        for(var j=1;j<=angka;j++){
-            if(i*j === angka){
-                temp=i.toString()+j.toString();
-                pengali.push(temp);
-                //console.log(temp)
-            }
+  // you can only write your code here!
+  var temp = ''
+  var pengali = []
+  for(var i = 1; i <= angka; i++){
+     for(var j = 1; j <= angka; j++){
+        if(i * j == angka){
+           temp = i.toString() + j.toString()
+           pengali.push(temp)
         }
-    }
-    
-    var digit=pengali[0].length;
-        //console.log(digit);
-        for(var i=0; i<pengali.length;i++){
-            if(digit > pengali[i].length){
-                digit=pengali[i].length;
-            }
-        }
-        return digit;
-    }
+     }
+  }
   
-  // TEST CASES
-  console.log(digitPerkalianMinimum(24)); // 2
-  console.log(digitPerkalianMinimum(90)); // 3
-  console.log(digitPerkalianMinimum(20)); // 2
-  console.log(digitPerkalianMinimum(179)); // 4
-  console.log(digitPerkalianMinimum(1)); // 2 
+  var digit = pengali[0].length
+  for(var k = 0; k < pengali.length; k++){
+     if(digit > pengali[k].length){
+        digit = pengali[k].length
+     }
+  }
+  return digit
+}
+
+// TEST CASES
+console.log(digitPerkalianMinimum(24)); // 2
+console.log(digitPerkalianMinimum(90)); // 3
+console.log(digitPerkalianMinimum(20)); // 2
+console.log(digitPerkalianMinimum(179)); // 4
+console.log(digitPerkalianMinimum(1)); // 2
