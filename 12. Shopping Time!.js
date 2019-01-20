62b8f918de8706444fbdb674af3d40e89b5160f3
rizalasnawi <44157537+rizalasnawi@users.noreply.github.com>
2019-01-20 16:56:35 +0700

    update

--- 12. Shopping Time!.js
+++ 12. Shopping Time!.js
@@ -1,50 +1,48 @@
 function shoppingTime(memberId, money) {
   // you can only write your code here!
-  var listBarang = []
-  listBarang.push(['Sepatu brand Stacattu', 1500000]);
-  listBarang.push(['Baju brand Zoro', 500000]);
-  listBarang.push(['Baju brand H&N', 250000]);
-  listBarang.push(['Sweater brand Uniklooh', 175000]);
-  listBarang.push(['Casing Handphone', 50000]);
+  var listBarang = [  
+    ['Sepatu brand Stacattu', 1500000],
+    ['Baju brand Zoro', 500000],
+    ['Baju brand H&N', 250000],
+    ['Sweater brand Uniklooh', 175000],
+    ['Casing Handphone', 50000]
+  ];
 
-  var data={}
+  var data = {
+    memberId : memberId,
+    money : money,
+    listPurchased : [],
+    changeMoney : 0
+  };
 
-  if(memberId === '' || memberId === undefined){
+  if (memberId === '' || memberId === undefined) {
       return "Mohon maaf, toko X hanya berlaku untuk member saja"
   }
-  if(money < 50000 ){
+  if (money < 50000) {
       return "Mohon maaf, uang tidak cukup"
   }
 
-  data['memberId'] = memberId;
-  data['money'] = money;
-
-  var Keranjang = [];
-  for(i=0;i<=listBarang.length-1;i++){
-      var change = money;
-      if(money >= listBarang[i][1]){
-          Keranjang.push(listBarang[i][0]);
-          money = change - listBarang[i][1];
-      }
-      data['listPurchased']=Keranjang;
-      data['changeMoney']=money;
+  for (i = 0; i < listBarang.length; i++) {
+    if (money >= listBarang[i][1]) {
+       data.listPurchased.push(listBarang[i][0])
+       money -= listBarang[i][1]
+    }
+    data.changeMoney = money
   }
-
-return data;
-
+  return data
 }
 
 // TEST CASES
 console.log(shoppingTime('1820RzKrnWn08', 2475000));
-  //{ memberId: '1820RzKrnWn08',
-  // money: 2475000,
-  // listPurchased:
-  //  [ 'Sepatu Stacattu',
-  //    'Baju Zoro',
-  //    'Baju H&N',
-  //    'Sweater Uniklooh',
-  //    'Casing Handphone' ],
-  // changeMoney: 0 }
+//{ memberId: '1820RzKrnWn08',
+// money: 2475000,
+// listPurchased:
+//  [ 'Sepatu Stacattu',
+//    'Baju Zoro',
+//    'Baju H&N',
+//    'Sweater Uniklooh',
+//    'Casing Handphone' ],
+// changeMoney: 0 }
 console.log(shoppingTime('82Ku8Ma742', 170000));
 //{ memberId: '82Ku8Ma742',
 // money: 170000,
