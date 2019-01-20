47ad7567fca0001c2be7901103fac8c72223dfaf
rizalasnawi <44157537+rizalasnawi@users.noreply.github.com>
2019-01-20 15:24:10 +0700

    update

--- 1. Angka Prima.js
+++ 1. Angka Prima.js
@@ -1,22 +1,22 @@
 function angkaPrima(angka) {
   // you can only write your code here!
-
-  for(var i=2; i<=angka; i++){
-      var isCheck=true;
-      for(var j=2; j<i; j++){
-          if(i%j==0){
-              isCheck = false;
-              break;
-          }
+  for(var i = 2; i <= angka; i++){
+     var isCheck = true
+     for(var j = 2; j < i; j++){
+        if(i % j == 0){
+           isCheck = false
+           break
+        }
       }
-      
     }
+
     if(isCheck){
-        return true;
-    }else{
-        return false;
+        return true
+    } else {
+        return false
     }
 }
+
 
 // TEST CASES
 console.log(angkaPrima(3)); // true
