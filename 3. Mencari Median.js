5ad1fc640373659752b49273995b72045d6a6a50
rizalasnawi <44157537+rizalasnawi@users.noreply.github.com>
2019-01-20 16:11:09 +0700

    update

--- 3. Mencari Median.js
+++ 3. Mencari Median.js
@@ -1,20 +1,19 @@
 function cariMedian(arr) {
   // you can only write your code here!
-  var median = 0;
- 
-  for(var i=0; i< arr.length; i++){
-      if(arr.length % 2 == 0){
-        median = (arr[arr.length/2] + arr[arr.length/2 -1])/2
-      }else{
-          median = arr[(Math.floor(arr.length/2))]
-          //console.log(median)
-      }
+  var median = 0
+
+  for(var i = 0; i < arr.length; i++){
+     if(arr.length % 2 == 0){
+        median = (arr[arr.length / 2] + arr[arr.length / 2 - 1]) / 2
+     } else {
+        median = arr[(Math.round(arr.length / 2))]
+     }
   }
-  return median;
+  return median
 }
 
 // TEST CASES
-console.log(cariMedian([1, 2, 3, 4, 5, 6, 7])); // 3
+console.log(cariMedian([1, 2, 3, 4, 5])); // 3
 console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
 console.log(cariMedian([3, 4, 7, 6, 10])); // 7
 console.log(cariMedian([1, 3, 3])); // 3
