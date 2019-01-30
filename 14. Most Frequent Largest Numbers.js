a5637f98a9408b52552ce1ab6b8cdfe0647cd224
rizalasnawi <44157537+rizalasnawi@users.noreply.github.com>
2019-01-30 14:56:43 +0700

    update

--- 14. Most Frequent Largest Numbers.js
+++ 14. Most Frequent Largest Numbers.js
@@ -1,47 +1,49 @@
 function sorting(arrNumber) {
     // code di sini
-    for(var i=0;i<arrNumber.length;i++){
-        for(var j=0;j<arrNumber.length-1;j++){
-            if(arrNumber[j] < arrNumber[j+1]){
-            var temp=arrNumber[j];
-            arrNumber[j]=arrNumber[j+1];
-            arrNumber[j+1]=temp;
+    for (var i = 0; i < arrNumber.length; i++) {
+        for (var j = 0; j < arrNumber.length - 1; j++) {
+            if (arrNumber[j] < arrNumber[j + 1]) {
+                var temp = arrNumber[j]
+                arrNumber[j] = arrNumber[j + 1]
+                arrNumber[j + 1] = temp
             }
         }
     }
-    return arrNumber;
-  }
-  
-   function getTotal(arrNumber) {
+    return arrNumber
+}
+
+function getTotal(arrNumber) {
     // code di sini
-    var max = arrNumber[0];
-    var counter = 0;
-    if(arrNumber.length==0 || arrNumber.length == undefined){
+    var max = arrNumber[0]
+    var counter = 0
+    if (arrNumber.length == 0 || arrNumber.length == undefined) {
         return '//'
     }
-    for(var i=0;i<=arrNumber.length-1;i++){
-        if(arrNumber[i] == max){
-            counter++;
+    for (var i = 0; i < arrNumber.length; i++) {
+        if (arrNumber[i] == max) {
+            counter++
         }
-        
+
     }
-    return "angka paling besar adalah " + max+ " dan jumlah kemunculan sebanyak " + counter+ " kali"
-  }
-  
-  function mostFrequentLargestNumbers(arrNumber) {
-    var listSort = sorting(arrNumber);
-    var countHighest = getTotal(listSort);
-    return countHighest;
-  }
-  
-  console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
-  //'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
-  
-  console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
-  //'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
-  
-  console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
-  //'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
-  
-  console.log(mostFrequentLargestNumbers([]));
-  //'' 
+    return "angka paling besar adalah " + max + " dan jumlah kemunculan sebanyak " + counter + " kali"
+}
+
+function mostFrequentLargestNumbers(arrNumber) {
+    
+    var listSort = sorting(arrNumber)
+    var countHighest = getTotal(listSort)
+    
+    return countHighest
+}
+
+console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
+//'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
+
+console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
+//'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
+
+console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
+//'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
+
+console.log(mostFrequentLargestNumbers([]));
+//''
