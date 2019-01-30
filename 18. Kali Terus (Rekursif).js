311983c8159eb44084777aec19299e12af436649
rizalasnawi <44157537+rizalasnawi@users.noreply.github.com>
2019-01-30 15:00:58 +0700

    update

--- 18. Kali Terus (Rekursif).js
+++ 18. Kali Terus (Rekursif).js
@@ -1,22 +1,21 @@
 function kaliTerusRekursif(angka) {
     // you can only write your code here!
-    var strAngka=String(angka)
+    var strAngka = String(angka)
     var hasil = 1
-    if(strAngka.length==1){
+    if (strAngka.length == 1) {
         return Number(strAngka)
-    }else{
-        
-        for(var i=0;i<strAngka.length;i++){
-            hasil = hasil * Number (strAngka[i])
-            }
+    } else {
+        for (var i = 0; i < strAngka.length; i++) {
+            hasil *= Number(strAngka[i])
         }
-    return kaliTerusRekursif(hasil);
+    }
+    return kaliTerusRekursif(hasil)
 }
 
-  
-  // TEST CASES
-  console.log(kaliTerusRekursif(66)); // 8
-  console.log(kaliTerusRekursif(3)); // 3
-  console.log(kaliTerusRekursif(24)); // 8
-  console.log(kaliTerusRekursif(654)); // 0
-  console.log(kaliTerusRekursif(1231)); // 6
+
+// TEST CASES
+console.log(kaliTerusRekursif(66)); // 8
+console.log(kaliTerusRekursif(3)); // 3
+console.log(kaliTerusRekursif(24)); // 8
+console.log(kaliTerusRekursif(654)); // 0
+console.log(kaliTerusRekursif(1231)); // 6
